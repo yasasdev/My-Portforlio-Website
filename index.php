1303f52581f780f2868e148b88541e2ded594511
Yasas Lekamge <yasaslekamge@gmail.com>
2024-11-10 21:24:03 +0530

    Index and css files update.

--- index.php
+++ index.php
@@ -4,8 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yasas Lekamge</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+    <!-- CSS Files -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/glightbox@3.0.6/dist/css/glightbox.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/swiper@8.4.5/swiper-bundle.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
+    <script src="https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js"></script>
+
+    <!-- Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
 </head>
 <body>
     
+    <!-- header -->
+    <header id="header" class="header dark-background d-flex flex-column">
+    <div class="profile-img">
+            <img src="assets/img/Yasas-Lekamge.jpg" alt="" class="img-fluid rounded-circle">
+        </div>
+
+        <a href="index.html" class="logo">
+            <h1 class="sitename">Yasas Lekamge</h1>
+        </a><br>
+
+        <div class="social-links">
+            <a href="https://x.com/YasasLekamge?t=d9KdCoUU8F2140cbzwGAgQ&s=09" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100082016200193&mibextid=JRoKGi" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/_yasas___?igsh=MWdqcGFqemlmdTVvdQ==" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/yasasdev" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div> <br> <br>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+              <li><a href="#hero" class="active"><i class="bi bi-house navicon"></i>Home</a></li>
+              <li><a href="#about"><i class="bi bi-person navicon"></i> About</a></li>
+              <li><a href="#skills"><i class="bi bi-hdd-stack navicon"></i> Skills</a></li>
+              <li><a href="#resume"><i class="bi bi-file-earmark-text navicon"></i> Resume</a></li>
+              <li><a href="#portfolio"><i class="bi bi-images navicon"></i> Portfolio</a></li>
+              <li><a href="#contact"><i class="bi bi-envelope navicon"></i> Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
 </body>
 </html>
